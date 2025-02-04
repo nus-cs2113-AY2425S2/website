0abe02f1c8db20105d5cde37776ef4029464da66
okkhoy <okkhoy@gmail.com>
2025-02-04 19:52:40 +0800

    Site Update. [skip ci]

--- schedule/week6/admin.page-vue-render.js
+++ schedule/week6/admin.page-vue-render.js
@@ -32,6 +32,6 @@ with(this){return _c('span',{staticClass:"badge bg-secondary  font-weight-normal
 with(this){return _c('div',[_c('ul',[_c('li',[_c('strong',[_v("An anonymous survey to submit feedback about the course and the teaching team")]),_v(" will open on Canvas by Monday. Please take a few minutes to give us your feedback.")])])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v(", generated on Tue, 4 Feb 2025, 19:33:25 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.5.3")]),_v(", generated on Tue, 4 Feb 2025, 19:48:27 GMT+8]")])])])}
 }];
   
